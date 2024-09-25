404d16ca7a87b47c17592295c79a434cee938be7
Jarrett Gaither <jarrett.gaither@liatrio.com>
2024-09-25 13:30:59 -0700

    Changing server to serve static html from public

--- index.js
+++ index.js
@@ -2,18 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-
-    const data = {
-        message: 'Pickup Team',
-    };
-
-    res.json(data);
-
-});
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
 app.listen(port, () => {
-
-    console.log('Web application is listening on port ' + port);
-
+  console.log(`Server is running on http://localhost:${port}`);
 });
